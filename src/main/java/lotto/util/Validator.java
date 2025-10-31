f6996ba345de46ae9e52c3a98bfb07ebb6520065
khcho96 <khcho1492@gmail.com>
2025-11-01 01:07:00 +0900

    style(Validator): 클래스 블럭 맨 위 공백 라인 추가

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -3,6 +3,7 @@ package lotto.util;
 import static lotto.constant.ErrorMessage.INPUT_NULL_OR_BLANK_ERROR;
 
 public class Validator {
+
     public static void validateNullOrBlank(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException(INPUT_NULL_OR_BLANK_ERROR.getErrorMessage());
