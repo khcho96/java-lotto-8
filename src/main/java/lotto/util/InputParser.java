d87debc7b3ce4dc073da1f085341236e61766ae6
khcho96 <khcho1492@gmail.com>
2025-11-01 02:33:30 +0900

    feat(InputParser): 당첨 번호에 대한 입력값을 검증하고 쉼표로 분리하는 기능 구현

--- src/main/java/lotto/util/InputParser.java
+++ src/main/java/lotto/util/InputParser.java
@@ -2,7 +2,12 @@ package lotto.util;
 
 import static lotto.constant.ErrorMessage.NUMBER_FORMAT_ERROR;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public class InputParser {
+
+    private static final String DELIMITER = ",";
 
     public static Integer parsePurchaseAmount(String rawPurchaseAmount) {
         Validator.validateNullOrBlank(rawPurchaseAmount);
@@ -12,5 +17,15 @@ public class InputParser {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NUMBER_FORMAT_ERROR.getErrorMessage());
         }
+    }
+
+    public static List<Integer> parseWinningLottoNumber(String rawWinningLottoNumber) {
+        Validator.validateNullOrBlank(rawWinningLottoNumber);
+        rawWinningLottoNumber = rawWinningLottoNumber.strip();
+        Validator.validateCsvFormat(rawWinningLottoNumber);
+        return Stream.of(rawWinningLottoNumber.split(DELIMITER))
+                .map(String::strip)
+                .map(Integer::parseInt)
+                .toList();
     }
 }
