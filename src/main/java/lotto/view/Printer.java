19eb9550a972fb718ca7c56612863e581e9c08cc
khcho96 <khcho1492@gmail.com>
2025-11-01 00:09:29 +0900

    refactor(Printer): 클래스명 및 변수명 변경
    purchasedAmount -> purchaseAmount

--- src/main/java/lotto/view/Printer.java
+++ src/main/java/lotto/view/Printer.java
@@ -7,12 +7,12 @@ import lotto.dto.LottoResultDto;
 public class Printer {
 
     private static final String NEW_LINE = System.lineSeparator();
-    private static final String PURCHASED_AMOUNT_REQUEST = "구입금액을 입력해 주세요.";
+    private static final String PURCHASE_AMOUNT_REQUEST = "구입금액을 입력해 주세요.";
     private static final String WINNING_LOTTO_REQUEST = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_REQUEST = "보너스 번호를 입력해 주세요.";
 
-    public static void printPurchasedAmountRequest() {
-        System.out.println(PURCHASED_AMOUNT_REQUEST);
+    public static void printPurchaseAmountRequest() {
+        System.out.println(PURCHASE_AMOUNT_REQUEST);
     }
 
     public static void printWinningLottoRequest() {
