dcca011efbce5c3201eda9cdace1e33eb44b4ed0
khcho96 <khcho1492@gmail.com>
2025-11-01 00:18:46 +0900

    feat(RandomLottoGenerator): 랜덤 함수 구현

---
+++ src/main/java/lotto/util/random/RandomLottoGenerator.java
@@ -0,0 +1,12 @@
+package lotto.util.random;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class RandomLottoGenerator implements LottoGenerator {
+
+    @Override
+    public List<Integer> generateLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+}
