b3e127a43f026b57a1c8a6a35c80799043996249
khcho96 <khcho1492@gmail.com>
2025-10-31 16:29:32 +0900

    feat(Reader): 사용자로부터 입력값 받는 기능 구현

---
+++ src/main/java/lotto/view/Reader.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Reader {
+
+    public static String readInput() {
+        return Console.readLine();
+    }
+}
