fdef1eeb4373204d4e8bb2161a901d2211ae17bc
khcho96 <khcho1492@gmail.com>
2025-11-01 00:16:26 +0900

    feat(LottoGenerator): 랜덤 함수 인터페이스 생성

--- src/main/java/lotto/util/random/LottoGenerator.java
+++ src/main/java/lotto/util/random/LottoGenerator.java
@@ -1,11 +1,8 @@
 package lotto.util.random;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
-public class LottoGenerator {
+public interface LottoGenerator {
 
-    public static List<Integer> generateLottoNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
-    }
+    List<Integer> generateLottoNumbers();
 }
