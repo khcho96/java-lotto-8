e183751bf5c190ee697d2e0eda45742d58a8e4be
khcho96 <khcho1492@gmail.com>
2025-10-31 20:09:27 +0900

    test(IssuedLottosTest): 여러 로또 번호 발행 단위 테스트 코드 구현

---
+++ src/test/java/lotto/domain/IssuedLottosTest.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.dto.IssuedLottosDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class IssuedLottosTest {
+
+    @DisplayName("로또 생성 테스트")
+    @Test
+    void 로또_생성_테스트() {
+        // given
+        IssuedLottos issuedLottos = IssuedLottos.getInstance();
+
+        // when
+        issuedLottos.add(new ArrayList<>(List.of(6, 2, 3, 1, 5, 4)));
+        issuedLottos.add(new ArrayList<>(List.of(5, 23, 12, 6, 8, 19)));
+        issuedLottos.add(new ArrayList<>(List.of(34, 25, 21, 1, 4, 9)));
+        issuedLottos.add(new ArrayList<>(List.of(14, 15, 41, 42, 30, 11)));
+        IssuedLottosDto issuedLottosDto = issuedLottos.getIssuedLottos();
+        List<List<Integer>> lottoNumbers = issuedLottosDto.issuedLottos();
+
+        // then
+        assertThat(lottoNumbers).containsExactly(
+                List.of(1, 2, 3, 4, 5, 6),
+                List.of(5, 6, 8, 12, 19, 23),
+                List.of(1, 4, 9, 21, 25, 34),
+                List.of(11, 14, 15, 30, 41, 42)
+        );
+    }
+}
