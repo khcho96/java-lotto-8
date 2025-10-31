beb23142f187b352323aa0a764eefcf5c301d0e1
khcho96 <khcho1492@gmail.com>
2025-11-01 02:34:18 +0900

    feat(Validator): 쉼표 구분 형식 검증 기능 구현

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -1,12 +1,21 @@
 package lotto.util;
 
+import static lotto.constant.ErrorMessage.CSV_FORMAT_ERROR;
 import static lotto.constant.ErrorMessage.INPUT_NULL_OR_BLANK_ERROR;
 
 public class Validator {
 
+    private static final String CSV_FORMAT = "^\\d+ *(, *\\d+ *)*,? *\\d+$";
+
     public static void validateNullOrBlank(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException(INPUT_NULL_OR_BLANK_ERROR.getErrorMessage());
+        }
+    }
+
+    public static void validateCsvFormat(String input) {
+        if (!input.matches(CSV_FORMAT)) {
+            throw new IllegalArgumentException(CSV_FORMAT_ERROR.getErrorMessage());
         }
     }
 }
