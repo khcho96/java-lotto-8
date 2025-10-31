12be16fe2f9132bdd49c2b0a946e264016c010b9
khcho96 <khcho1492@gmail.com>
2025-10-31 18:38:24 +0900

    feat(LottoMachine): 로또를 발행한 결과를 DTO로 반환하는 기능 구현

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,5 +1,8 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import lotto.dto.IssuedLottosDto;
 import lotto.vo.PurchasedAmount;
 
 public class LottoMachine {
@@ -10,5 +13,14 @@ public class LottoMachine {
     public LottoMachine(Integer purchasedAmount) {
         this.purchasedAmount = new PurchasedAmount(purchasedAmount);
         lottoCount = this.purchasedAmount.getLottoCount();
+    }
+
+    public IssuedLottosDto generateLottos() {
+        IssuedLottos issuedLottos = IssuedLottos.getInstance();
+        for (int count = 0; count < lottoCount; count++) {
+            List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            issuedLottos.add(lottoNumbers);
+        }
+        return issuedLottos.getIssuedLottos();
     }
 }
