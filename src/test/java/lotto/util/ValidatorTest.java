223ca32115e8d4a9d4b3a561afc92e7d09817e5a
khcho96 <khcho1492@gmail.com>
2025-11-01 01:38:57 +0900

    test(ValidatorTest): 쉼표 구분 입력 검증 단위 테스트 코드 구현

--- src/test/java/lotto/util/ValidatorTest.java
+++ src/test/java/lotto/util/ValidatorTest.java
@@ -1,5 +1,6 @@
 package lotto.util;
 
+import static lotto.constant.ErrorMessage.CSV_FORMAT_ERROR;
 import static lotto.constant.ErrorMessage.INPUT_NULL_OR_BLANK_ERROR;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -18,5 +19,14 @@ class ValidatorTest {
         assertThatThrownBy(()->Validator.validateNullOrBlank(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(INPUT_NULL_OR_BLANK_ERROR.getErrorMessage());
+    }
+
+    @DisplayName("입력값이 쉼표로 구분된 정수 형식이 아니면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {",1,2,3", "1,2,3,", ",1,2,3,", "1,,2", "1.2.3", "1|2|3"})
+    void 입력값이_쉼표로_구분된_정수_형식이_아니면_예외를_발생시킨다(String input) {
+        assertThatThrownBy(()->Validator.validateCsvFormat(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(CSV_FORMAT_ERROR.getErrorMessage());
     }
 }
