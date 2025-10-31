21a83dafa4123e9dc16140bcf8dc6b9aff25be09
khcho96 <khcho1492@gmail.com>
2025-10-31 20:07:01 +0900

    feat(Printer): 발행한 로또 번호 출력 기능 구현

--- src/main/java/lotto/view/Printer.java
+++ src/main/java/lotto/view/Printer.java
@@ -1,10 +1,12 @@
 package lotto.view;
 
+import java.util.List;
 import lotto.dto.IssuedLottosDto;
 import lotto.dto.LottoResultDto;
 
 public class Printer {
 
+    private static final String NEW_LINE = System.lineSeparator();
     private static final String PURCHASED_AMOUNT_REQUEST = "구입금액을 입력해 주세요.";
     private static final String WINNING_LOTTO_REQUEST = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_REQUEST = "보너스 번호를 입력해 주세요.";
@@ -22,6 +24,11 @@ public class Printer {
     }
 
     public static void printIssuedLottos(IssuedLottosDto issuedLottosDto) {
+        List<List<Integer>> issuedLottos = issuedLottosDto.issuedLottos();
+        System.out.println(NEW_LINE + issuedLottos.size() + "개를 구매했습니다.");
+        for (List<Integer> issuedLotto : issuedLottos) {
+            System.out.println(issuedLotto);
+        }
     }
 
     public static void printLottoResult(LottoResultDto lottoResultDto) {
