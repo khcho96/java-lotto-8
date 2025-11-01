eb70e2b79c72b4378a925df8b5dfd4f26ace472e
khcho96 <khcho1492@gmail.com>
2025-11-01 19:35:24 +0900

    refactor(PurchaseAmount): 객체 재사용 위한 싱글톤 패턴 구현

---
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -0,0 +1,47 @@
+package lotto.domain;
+
+import static lotto.constant.ErrorMessage.PURCHASE_AMOUNT_RANGE_ERROR;
+import static lotto.constant.ErrorMessage.PURCHASE_AMOUNT_UNIT_ERROR;
+
+public class PurchaseAmount {
+
+    private static PurchaseAmount purchaseAmountObj;
+    private final Integer purchaseAmount;
+
+    public PurchaseAmount(Integer purchaseAmount) {
+        validateRange(purchaseAmount);
+        validateUnit(purchaseAmount);
+        this.purchaseAmount = purchaseAmount;
+    }
+
+    public static PurchaseAmount from(Integer purchaseAmount) {
+        if (purchaseAmountObj == null) {
+            purchaseAmountObj = new PurchaseAmount(purchaseAmount);
+        }
+        return purchaseAmountObj;
+    }
+
+    public static PurchaseAmount getInstance() {
+        return purchaseAmountObj;
+    }
+
+    private void validateRange(Integer purchaseAmount) {
+        if (0 >= purchaseAmount || purchaseAmount > 100000) {
+            throw new IllegalArgumentException(PURCHASE_AMOUNT_RANGE_ERROR.getErrorMessage());
+        }
+    }
+
+    private void validateUnit(Integer purchaseAmount) {
+        if (purchaseAmount % 1000 != 0) {
+            throw new IllegalArgumentException(PURCHASE_AMOUNT_UNIT_ERROR.getErrorMessage());
+        }
+    }
+
+    public Integer getLottoCount() {
+        return purchaseAmount / 1000;
+    }
+
+    public Double getProfitRate(Long profit) {
+        return (double) profit / purchaseAmount * 100;
+    }
+}
