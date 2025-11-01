205db70c33aa2d2f865f1242761506ab64d23215
khcho96 <khcho1492@gmail.com>
2025-11-01 19:32:16 +0900

    feat(WinningLotto): 각 로또의 결과를 결정하는 기능 구현

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -4,6 +4,7 @@ import static lotto.constant.ErrorMessage.BONUS_NUMBER_UNIQUE_ERROR;
 import static lotto.constant.ErrorMessage.LOTTO_NUMBER_RANGE_ERROR;
 
 import java.util.List;
+import lotto.constant.core.Rank;
 
 public class WinningLotto {
     private final Lotto winningLotto;
@@ -33,5 +34,25 @@ public class WinningLotto {
         if (winningLotto.contains(bonusNumber)) {
             throw new IllegalArgumentException(BONUS_NUMBER_UNIQUE_ERROR.getErrorMessage());
         }
+    }
+
+    public Rank determineRank(Lotto issuedLotto) {
+        Integer matchedCount = winningLotto.getMatchedCount(issuedLotto);
+        if (matchedCount == 6) {
+            return Rank.FIRST;
+        }
+        if (matchedCount == 5 && issuedLotto.contains(bonusNumber)) {
+            return Rank.SECOND;
+        }
+        if (matchedCount == 5) {
+            return Rank.THIRD;
+        }
+        if (matchedCount == 4) {
+            return Rank.FOURTH;
+        }
+        if (matchedCount == 3) {
+            return Rank.FIFTH;
+        }
+        return null;
     }
 }
