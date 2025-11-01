30bea22ffdf301629dd5fafcd834e6e25d8bca56
khcho96 <khcho1492@gmail.com>
2025-11-01 19:36:01 +0900

    refactor(LottoMachine): PurchaseAmount 구현 변경으로 인한 코드 수정

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -3,16 +3,14 @@ package lotto.domain;
 import java.util.List;
 import lotto.dto.IssuedLottosDto;
 import lotto.util.random.LottoGenerator;
-import lotto.domain.vo.PurchaseAmount;
 
 public class LottoMachine {
 
-    private final PurchaseAmount purchaseAmount;
     private final Integer lottoCount;
 
     public LottoMachine(Integer purchaseAmount) {
-        this.purchaseAmount = new PurchaseAmount(purchaseAmount);
-        lottoCount = this.purchaseAmount.getLottoCount();
+        PurchaseAmount purchaseAmountObj = PurchaseAmount.from(purchaseAmount);
+        lottoCount = purchaseAmountObj.getLottoCount();
     }
 
     public IssuedLottosDto generateLottos(LottoGenerator lottoGenerator) {
