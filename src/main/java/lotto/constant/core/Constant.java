d635f534580f5b21318785bc708568e9907bab60
khcho96 <khcho1492@gmail.com>
2025-11-01 23:45:22 +0900

    feat(Constant): 상수 처리

---
+++ src/main/java/lotto/constant/core/Constant.java
@@ -0,0 +1,18 @@
+package lotto.constant.core;
+
+public class Constant {
+
+    public static final int PURCHASE_AMOUNT_MIN = 0;
+    public static final int PURCHASE_AMOUNT_MAX = 100000;
+    public static final int PURCHASE_AMOUNT_UNIT = 1000;
+
+    public static final int LOTTO_NUMBER_COUNT = 6;
+    public static final int LOTTO_NUMBER_MIN = 1;
+    public static final int LOTTO_NUMBER_MAX = 45;
+
+    public static final int FIRST_RANK_MATCHED_COUNT = 6;
+    public static final int SECOND_RANK_MATCHED_COUNT = 5;
+    public static final int THIRD_RANK_MATCHED_COUNT = 5;
+    public static final int FOURTH_RANK_MATCHED_COUNT = 4;
+    public static final int FIFTH_RANK_MATCHED_COUNT = 3;
+}
