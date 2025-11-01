666642d7568702f8151517e073f6a177dbf0e1a6
khcho96 <khcho1492@gmail.com>
2025-11-01 23:49:55 +0900

    style(Application): 공백 컨벤션 적용

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,7 @@ import lotto.controller.LottoController;
 import lotto.service.LottoService;
 
 public class Application {
+
     public static void main(String[] args) {
         LottoService lottoService = new LottoService();
         LottoController lottoController = new LottoController(lottoService);
