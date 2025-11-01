7b798aec49f4b715284a207d87844ab9e0039f0e
khcho96 <khcho1492@gmail.com>
2025-11-01 11:57:33 +0900

    feat(LottoService): 당첨번호와 보너스 번호를 등록하는 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,13 +1,26 @@
 package lotto.service;
 
+import java.util.List;
 import lotto.domain.LottoMachine;
+import lotto.domain.WinningLotto;
 import lotto.dto.IssuedLottosDto;
 import lotto.util.random.RandomLottoGenerator;
 
 public class LottoService {
+
+    private WinningLotto winningLotto;
+
     public IssuedLottosDto issueLottos(Integer purchaseAmount) {
         LottoMachine lottoMachine = new LottoMachine(purchaseAmount);
         RandomLottoGenerator lottoGenerator = new RandomLottoGenerator();
         return lottoMachine.generateLottos(lottoGenerator);
+    }
+
+    public void registerWinningLottoNumber(List<Integer> winningLottoNumber) {
+        winningLotto = WinningLotto.from(winningLottoNumber);
+    }
+
+    public void registerBonusNumber(Integer bonusNumber) {
+        winningLotto.registerBonusNumber(bonusNumber);
     }
 }
