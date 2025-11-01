dd0d565cccaf01a521073fbbe4fba426427b4a56
khcho96 <khcho1492@gmail.com>
2025-11-01 23:47:51 +0900

    refactor(RandomLottoGenerator): 상수 처리

--- src/main/java/lotto/util/random/RandomLottoGenerator.java
+++ src/main/java/lotto/util/random/RandomLottoGenerator.java
@@ -1,5 +1,9 @@
 package lotto.util.random;
 
+import static lotto.constant.core.Constant.LOTTO_NUMBER_COUNT;
+import static lotto.constant.core.Constant.LOTTO_NUMBER_MAX;
+import static lotto.constant.core.Constant.LOTTO_NUMBER_MIN;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
@@ -7,6 +11,6 @@ public class RandomLottoGenerator implements LottoGenerator {
 
     @Override
     public List<Integer> generateLottoNumbers() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX, LOTTO_NUMBER_COUNT);
     }
 }
