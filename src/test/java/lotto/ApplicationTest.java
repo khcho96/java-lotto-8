5b9f7cc6e97b5c7949b8249158fd2e68189bb6d6
khcho96 <khcho1492@gmail.com>
2025-11-02 11:07:18 +0900

    test(ApplicationTest): 정상 입력 케이스 추가

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -6,7 +6,6 @@ import lotto.domain.PurchaseAmount;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -27,43 +26,103 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ApplicationTest extends NsTest {
 
+    private final List<String> output = List.of(
+            "8개를 구매했습니다.",
+            "[8, 21, 23, 41, 42, 43]",
+            "[3, 5, 11, 16, 32, 38]",
+            "[7, 11, 16, 35, 36, 44]",
+            "[1, 8, 11, 31, 41, 42]",
+            "[13, 14, 16, 38, 42, 45]",
+            "[7, 11, 30, 40, 42, 43]",
+            "[2, 13, 22, 32, 38, 45]",
+            "[1, 3, 5, 14, 22, 45]",
+            "3개 일치 (5,000원) - 1개",
+            "4개 일치 (50,000원) - 0개",
+            "5개 일치 (1,500,000원) - 0개",
+            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+            "6개 일치 (2,000,000,000원) - 0개",
+            "총 수익률은 62.5%입니다."
+    );
+    private final List<List<Integer>> input = List.of(
+            List.of(8, 21, 23, 41, 42, 43),
+            List.of(3, 5, 11, 16, 32, 38),
+            List.of(7, 11, 16, 35, 36, 44),
+            List.of(1, 8, 11, 31, 41, 42),
+            List.of(13, 14, 16, 38, 42, 45),
+            List.of(7, 11, 30, 40, 42, 43),
+            List.of(2, 13, 22, 32, 38, 45),
+            List.of(1, 3, 5, 14, 22, 45)
+    );
+
     @AfterEach
     void tearDown() {
         IssuedLottos.resetForTest();
         PurchaseAmount.resetForTest();
     }
 
-    @Test
-    void 기능_테스트() {
+    @DisplayName("구입 금액 정상 입력 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"8000", "  8000  ", "\t8000\n"})
+    void 구입_금액_정상_입력_테스트(String purchaseAmount) {
         assertRandomUniqueNumbersInRangeTest(
                 () -> {
-                    run("8000", "1,2,3,4,5,6", "7");
+                    run(purchaseAmount, "1,2,3,4,5,6", "7");
                     assertThat(output()).contains(
-                            "8개를 구매했습니다.",
-                            "[8, 21, 23, 41, 42, 43]",
-                            "[3, 5, 11, 16, 32, 38]",
-                            "[7, 11, 16, 35, 36, 44]",
-                            "[1, 8, 11, 31, 41, 42]",
-                            "[13, 14, 16, 38, 42, 45]",
-                            "[7, 11, 30, 40, 42, 43]",
-                            "[2, 13, 22, 32, 38, 45]",
-                            "[1, 3, 5, 14, 22, 45]",
-                            "3개 일치 (5,000원) - 1개",
-                            "4개 일치 (50,000원) - 0개",
-                            "5개 일치 (1,500,000원) - 0개",
-                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
-                            "6개 일치 (2,000,000,000원) - 0개",
-                            "총 수익률은 62.5%입니다."
+                            output
                     );
                 },
-                List.of(8, 21, 23, 41, 42, 43),
-                List.of(3, 5, 11, 16, 32, 38),
-                List.of(7, 11, 16, 35, 36, 44),
-                List.of(1, 8, 11, 31, 41, 42),
-                List.of(13, 14, 16, 38, 42, 45),
-                List.of(7, 11, 30, 40, 42, 43),
-                List.of(2, 13, 22, 32, 38, 45),
-                List.of(1, 3, 5, 14, 22, 45)
+                input.get(0),
+                input.get(1),
+                input.get(2),
+                input.get(3),
+                input.get(4),
+                input.get(5),
+                input.get(6),
+                input.get(7)
+        );
+    }
+
+    @DisplayName("당첨 번호 정상 입력 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,6", "  1,2,3,4,5,6  ", " 1 , 2 , 3 , 4 , 5 , 6 ", "6,5,4,3,2,1"})
+    void 당첨_번호_정상_입력_테스트(String winningLottoNumber) {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("8000", winningLottoNumber, "7");
+                    assertThat(output()).contains(
+                            output
+                    );
+                },
+                input.get(0),
+                input.get(1),
+                input.get(2),
+                input.get(3),
+                input.get(4),
+                input.get(5),
+                input.get(6),
+                input.get(7)
+        );
+    }
+
+    @DisplayName("보너스 번호 정상 입력 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"7", "  7  ", "\n7\t"})
+    void 보너스_번호_정상_입력_테스트(String bonusNumber) {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("8000", "1,2,3,4,5,6", bonusNumber);
+                    assertThat(output()).contains(
+                            output
+                    );
+                },
+                input.get(0),
+                input.get(1),
+                input.get(2),
+                input.get(3),
+                input.get(4),
+                input.get(5),
+                input.get(6),
+                input.get(7)
         );
     }
 
