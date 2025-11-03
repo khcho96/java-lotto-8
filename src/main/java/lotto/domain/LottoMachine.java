6632ed1b6b6231f146996a666f6a57c56e31a2f6
khcho96 <khcho1492@gmail.com>
2025-11-03 11:55:10 +0900

    refactor(LottoMachine): 싱글톤 패턴 철회로 인한 코드 수정

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,28 +1,26 @@
 package lotto.domain;
 
 import java.util.List;
-import lotto.dto.IssuedLottosDto;
 import lotto.generator.LottoGenerator;
 
 public class LottoMachine {
 
     private final Integer lottoCount;
 
-    private LottoMachine(Integer purchaseAmount) {
-        PurchaseAmount purchaseAmountObj = PurchaseAmount.from(purchaseAmount);
-        lottoCount = purchaseAmountObj.getLottoCount();
+    private LottoMachine(PurchaseAmount purchaseAmount) {
+        lottoCount = purchaseAmount.getLottoCount();
     }
 
-    public static LottoMachine from(Integer purchaseAmount) {
+    public static LottoMachine from(PurchaseAmount purchaseAmount) {
         return new LottoMachine(purchaseAmount);
     }
 
-    public IssuedLottosDto generateLottos(LottoGenerator lottoGenerator) {
+    public IssuedLottos generateLottos(LottoGenerator lottoGenerator) {
         IssuedLottos issuedLottos = IssuedLottos.getInstance();
         for (int count = 0; count < lottoCount; count++) {
             List<Integer> lottoNumbers = lottoGenerator.generateLottoNumbers();
             issuedLottos.add(lottoNumbers);
         }
-        return issuedLottos.getIssuedLottos();
+        return issuedLottos;
     }
 }
